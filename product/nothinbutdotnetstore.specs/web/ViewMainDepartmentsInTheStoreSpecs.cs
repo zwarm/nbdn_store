644a35fa7a74753b11f1a74d44b019023762b63b
Annmarie Ziegler <annmarieziegler@nbty.com>
2010-12-09 11:11:34 -0500

    first

--- product/nothinbutdotnetstore.specs/web/ViewMainDepartmentsInTheStoreSpecs.cs
+++ product/nothinbutdotnetstore.specs/web/ViewMainDepartmentsInTheStoreSpecs.cs
@@ -1,3 +1,4 @@
+ using System.Web.UI.WebControls;
  using Machine.Specifications;
  using Machine.Specifications.DevelopWithPassion.Rhino;
  using nothinbutdotnetstore.web;
@@ -15,10 +16,25 @@ namespace nothinbutdotnetstore.specs.web
         }
 
         [Subject(typeof(ViewMainDepartmentsInTheStore))]
-        public class when_observation_name : concern
+        public class when_viewing_main_departments : concern
         {
-        
-                
+
+            Establish c = () =>
+            {
+                resquest = an<Request>();
+                response = an<Response>();
+                application_command = the_dependency<ViewRenderer>();
+            };
+
+            Because b = () =>
+                sut.process(request);
+
+            It should_delegate_the_view_rendering_to_the_view_render = () =>
+                view_renderer.received(x => x.Render());
+
+            static Response response;
+            static Request request;
+            static ViewRenderer view_renderer;  
         }
     }
 }
---
+++ product/nothinbutdotnetstore.specs/web/ViewRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetstore.specs.web
+{
+    public interface ViewRenderer
+    {
+        Response Render();
+    }
+}
